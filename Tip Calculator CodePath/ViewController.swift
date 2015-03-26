3e436bfd364fd1a0be99f4dec2ce5dd189c50b4a
Francisco <franciscodlp@gmail.com>
2015-03-25 21:06:13 -0700

    Initial Commit

---
+++ Tip Calculator CodePath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator CodePath
+//
+//  Created by Francisco de la Pena on 3/25/15.
+//  Copyright (c) 2015 ___TwisterLabs___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
